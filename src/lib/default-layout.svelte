9544c540c7a2f7020847c1862fb4069b7db34b61
BahaaZidan <gebnatorky@gmail.com>
2024-04-21 21:05:42 +0200

    include all past articles + tweek layout to make font larger

--- src/lib/default-layout.svelte
+++ src/lib/default-layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	export let title: string;
 	export let date: string;
+	export let data;
+	export let form;
 </script>
 
-<article class="prose">
+<article class="prose prose-lg p-4">
 	<h1>{title}</h1>
 	<h3>{date}</h3>
 	<div class="divider"></div>
