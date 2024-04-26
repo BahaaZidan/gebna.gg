6952c1cbefa0fd66e2a4671eb183311a7aa292f6
BahaaZidan <gebnatorky@gmail.com>
2024-04-26 20:07:36 +0300

    center home page too -_-

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,18 @@
 	<meta name="description" content="Gebna, GebnaTorky, or Bahaa Zidan" />
 </svelte:head>
 
-<div class="flex flex-col p-4 max-w-[65ch]">
-	{#each articles as article, index}
-		<a class="link-hover" href={article.path}>
-			<div class="text-4xl font-bold">
-				{article.title}
-			</div>
-			<div class="text-lg">{article.publishDate}</div>
-		</a>
-		{#if index !== articles.length - 1}
-			<div class="divider"></div>
-		{/if}
-	{/each}
+<div class="flex justify-center">
+	<div class="flex flex-col p-4 max-w-[65ch]">
+		{#each articles as article, index}
+			<a class="link-hover" href={article.path}>
+				<div class="text-4xl font-bold">
+					{article.title}
+				</div>
+				<div class="text-lg">{article.publishDate}</div>
+			</a>
+			{#if index !== articles.length - 1}
+				<div class="divider"></div>
+			{/if}
+		{/each}
+	</div>
 </div>
