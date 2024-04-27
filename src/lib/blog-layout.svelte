21b350fa91033ff70433264e7164fd8a7c7c817b
BahaaZidan <gebnatorky@gmail.com>
2024-04-27 07:47:40 +0300

    blog layout fix

--- src/lib/blog-layout.svelte
+++ src/lib/blog-layout.svelte
@@ -14,15 +14,13 @@
 	<meta name="description" content={description} />
 </svelte:head>
 
-<div class="flex justify-center">
-	<article class="prose prose-lg p-4">
-		<h1 style:--articleTitle="title_{slugify(title, { strict: true, lower: true })}">{title}</h1>
-		<h4>Written by <a href="/whoami">Bahaa Zidan</a></h4>
-		<h4>{date}</h4>
-		<div class="divider"></div>
-		<slot></slot>
-	</article>
-</div>
+<article class="prose prose-lg p-4 mx-auto">
+	<h1 style:--articleTitle="title_{slugify(title, { strict: true, lower: true })}">{title}</h1>
+	<h4>Written by <a href="/whoami">Bahaa Zidan</a></h4>
+	<h4>{date}</h4>
+	<div class="divider"></div>
+	<slot></slot>
+</article>
 
 <style>
 	article {
