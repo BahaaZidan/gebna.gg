d72094dd9206ae2756db8cfe68ee60bbb961b604
BahaaZidan <gebnatorky@gmail.com>
2024-05-03 20:47:30 +0300

    add open graph and twitter head tags to articles

--- src/lib/blog-layout.svelte
+++ src/lib/blog-layout.svelte
@@ -12,6 +12,12 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:type" content="website" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:creator" content="@GebnaTorky" />
+	<meta name="twitter:description" content={description} />
 </svelte:head>
 
 <article class="prose prose-lg p-4 mx-auto">
