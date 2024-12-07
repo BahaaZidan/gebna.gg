495986eea135523f0c5d129ab9684f6ad7030c35
BahaaZidan <gebnatorky@gmail.com>
2024-12-07 11:28:22 +0200

    initial search impl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gebna.gg",
-  integrations: [mdx(), sitemap(), tailwind()],
+  build: {
+    format: "file",
+  },
+  integrations: [mdx(), sitemap(), tailwind(), pagefind()],
   experimental: {
     svg: true,
   },
