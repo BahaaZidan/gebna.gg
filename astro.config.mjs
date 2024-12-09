a83301cf9bc6a34d5124e9b732966c9f0692b8b6
BahaaZidan <gebnatorky@gmail.com>
2024-12-09 12:05:26 +0200

    use rehype to turn section headings into links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+
 import pagefind from "astro-pagefind";
 
-// https://astro.build/config
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
 export default defineConfig({
   site: "https://gebna.gg",
   build: {
@@ -14,5 +17,17 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind(), pagefind(), svelte()],
   experimental: {
     svg: true,
+  },
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: { className: ["link-hover"] },
+        },
+      ],
+    ],
   },
 });
