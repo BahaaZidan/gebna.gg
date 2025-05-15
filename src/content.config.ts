42999e7c067058a517d265f6631fc16787d5f1ac
BahaaZidan <gebnatorky@gmail.com>
2025-05-15 14:32:13 +0300

    upgrade to content layer

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  type: "content",
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
