a9c5dfd886df45a90dc9ebcc14575b1892d4d0e7
BahaaZidan <gebnatorky@gmail.com>
2025-05-26 12:50:40 +0300

    script to convert all hero.{jpg|png} to hero.webp + rename to og.jpg

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,14 +3,11 @@ import { glob } from "astro/loaders";
 
 const blog = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
   }),
 });
 
