b364b555e51984fe25f7944e208d8758ee002125
BahaaZidan <gebnatorky@gmail.com>
2025-05-28 18:30:22 +0300

    change title

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = 'https://gebna.gg/';
-export const DEFAULT_TITLE = "Bahaa Zidan's Blog";
+export const DEFAULT_TITLE = 'Bahaa Zidan';
 export const DEFAULT_DESCRIPTION =
-	'Technical tips, tutorials, workshops, guides, showcases, and editorials';
+	'Technical tips, tutorials, workshops, guides, showcases, and editorials written by Bahaa Zidan';
