69ad0de1ebd5c6122073fec474850107807d93d7
BahaaZidan <gebnatorky@gmail.com>
2025-06-09 18:05:21 +0300

    gebna.gg instead of bsky

--- src/routes/blog/[id]/+page.svelte
+++ src/routes/blog/[id]/+page.svelte
@@ -104,7 +104,7 @@
 					</li>
 					<li>
 						<a
-							href={`https://bsky.app/intent/compose?text=${data.title} by @gebna.bsky.social ${data.canonicalURL}`}
+							href={`https://bsky.app/intent/compose?text=${data.title} by @gebna.gg ${data.canonicalURL}`}
 							target="_blank"
 							rel="noreferrer noopener"
 							data-umami-event="Share"
