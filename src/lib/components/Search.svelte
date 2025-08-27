eb96860b2263413cbcc2fb74170f6387dd1f6d72
BahaaZidan <gebnatorky@gmail.com>
2025-08-27 20:41:15 +0300

    pretty good search tbh

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import SearchIcon from '@lucide/svelte/icons/search';
+	import XIcon from '@lucide/svelte/icons/x';
 	import { onMount } from 'svelte';
 	// TODO: figure out a way to use the types directly from pagefind. we're not using this vite-plugin anyway
 	import type { Pagefind } from 'vite-plugin-pagefind/types';
@@ -26,45 +27,66 @@
 
 	let searchValue = $state('');
 	let searchResults = $derived(fetchSearchResults(searchValue));
+
+	let dialog: HTMLDialogElement;
+	let input: HTMLInputElement;
+
+	function showAndFocus() {
+		dialog.showModal();
+		setTimeout(() => input.focus(), 50);
+	}
 </script>
 
-<div class="drawer w-28">
-	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content">
-		<label for="my-drawer" class="btn drawer-button">
-			<SearchIcon />
-			Search
-		</label>
-	</div>
-	<div class="drawer-side">
-		<label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-		<ul class="menu bg-base-200 text-base-content min-h-full w-80 gap-2 p-4">
-			<div class="revert-all">
-				<div class="flex max-w-80 flex-col gap-6">
-					<input
-						type="text"
-						placeholder="search"
-						class="input input-bordered input-lg"
-						bind:value={searchValue}
-					/>
+<svelte:document
+	onkeypress={(e) => {
+		if (e.key === '/') {
+			showAndFocus();
+		}
+	}}
+/>
 
-					{#await searchResults}
-						<div>loading...</div>
-					{:then results}
-						{#each results as result (result.url)}
-							<div class="flex flex-col gap-1">
-								<a
-									href={result.url.replace('.html', '')}
-									class="link-hover text-sm font-bold wrap-anywhere"
-								>
-									{result.meta.title}
-								</a>
-								<div class="text-xs">{@html result.excerpt}</div>
-							</div>
-						{/each}
-					{/await}
-				</div>
-			</div>
-		</ul>
-	</div>
+<!-- svelte-ignore a11y_interactive_supports_focus -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<div class="input hover:cursor-text" role="button" onclick={showAndFocus}>
+	<SearchIcon />
+	<div class="grow">Search</div>
+	<kbd class="kbd">/</kbd>
 </div>
+<dialog bind:this={dialog} class="modal">
+	<div class="modal-box h-[36rem] w-xl">
+		<div class="flex flex-col gap-4">
+			<div class="join">
+				<input
+					type="text"
+					placeholder="Search..."
+					class="input input-bordered input-lg join-item grow"
+					bind:value={searchValue}
+					bind:this={input}
+				/>
+				<form method="dialog">
+					<button class="btn btn-lg join-item"><XIcon /></button>
+				</form>
+			</div>
+
+			{#await searchResults}
+				<div>loading...</div>
+			{:then results}
+				{#each results as result (result.url)}
+					<div class="bg-base-300 flex flex-col gap-1 rounded-lg p-4">
+						<a
+							data-sveltekit-reload
+							href={result.url.replace('.html', '')}
+							class="link-hover text-sm font-bold wrap-anywhere"
+						>
+							{result.meta.title}
+						</a>
+						<div class="text-xs">{@html result.excerpt}</div>
+					</div>
+				{/each}
+			{/await}
+		</div>
+	</div>
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
