692a42cd18af7299ef334148be70a6ff4eed5f02
BahaaZidan <gebnatorky@gmail.com>
2025-09-25 18:19:42 +0300

    show a list of all tags in the home page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,6 +4,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = () => {
 	const posts = getBlogPostsMetadata();
+	const tags = Array.from(new Set(posts.map((p) => p.tags || []).flat()));
 
-	return { posts };
+	return { posts, tags };
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,12 @@
 	imagePath="/avatar.jpeg"
 />
 
-<main class="flex justify-center">
+<main class="flex flex-col items-center">
+	<div class="flex max-w-7xl flex-wrap gap-2 px-4">
+		{#each data.tags as tag (tag)}
+			<a class="link-hover badge" href="/blog/tag/{tag}">#{tag}</a>
+		{/each}
+	</div>
 	<section class="my-4 flex max-w-7xl flex-wrap justify-center gap-4">
 		{#each data.posts as post (post.id)}
 			<PostCard {post} />
