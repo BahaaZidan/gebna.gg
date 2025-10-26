4ac21a872d431e14d728bdd9f3c1e40a392238be
BahaaZidan <gebnatorky@gmail.com>
2025-10-26 17:54:13 +0300

    404 ?

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,12 @@
-<script>
+<script lang="ts">
 	import { resolve } from '$app/paths';
 </script>
 
-<!-- WORKAROUND: make it work w/ cf pages fallback behaviour -->
-
 <div class="flex min-h-[calc(100vh-8rem)] items-center justify-center p-4">
 	<div class="text-center">
-		<h1 class="text-primary text-9xl font-bold">404</h1>
+		<h1 class="text-9xl font-bold text-primary">404</h1>
 		<p class="mt-6 text-2xl font-semibold md:text-3xl">Page Not Found</p>
-		<p class="text-base-content mt-4 mb-8">Sorry, the page you're looking for doesn't exist.</p>
+		<p class="mt-4 mb-8 text-base-content">Sorry, the page you're looking for doesn't exist.</p>
 		<a href={resolve('/')} class="btn btn-primary">Go Home</a>
 	</div>
 </div>
